941ed6a08ea8d1479b10f7ee290d2508f2e27b42
samia <sma225@njit.edu>
2019-11-13 16:51:06 -0500

    filepath edit in Statistics class

--- Statistics/Statistics.py
+++ Statistics/Statistics.py
@@ -13,7 +13,7 @@ class Statistics(Calculator):
     data = []
 
     def __init__(self, filepath):
-        self.data = CsvReader(filepath)
+        self.data = CsvReader('/Tests/Data/Stat.csv')
         super().__init__()
 
     def mean(self):
